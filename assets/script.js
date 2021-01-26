d4dab4333397200ab331d8fc94a8daf5c62bcc26
Jesus Milan <jmilan.gonzalez@gmail.com>
2021-01-25 22:17:53 -0500

    made my search button work

---
+++ assets/script.js
@@ -0,0 +1,17 @@
+// start of jQuery
+$(document).ready(function(){
+
+// global variable setting
+
+
+// search button action
+$("#btn-search").on("click", function(){
+    let citySearch = $("#city-search").val();
+    console.log(citySearch)
+});
+
+// do this when the search button is clicked
+
+
+
+});
