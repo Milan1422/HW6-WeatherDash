d102c8b879c595b330179d9c232d72dfc480fb8a
Jesus Milan <jmilan.gonzalez@gmail.com>
2021-01-25 22:50:52 -0500

    teating gettin results back from open weather

--- assets/script.js
+++ assets/script.js
@@ -3,15 +3,27 @@ $(document).ready(function(){
 
 // global variable setting
 
-
 // search button action
 $("#btn-search").on("click", function(){
     let citySearch = $("#city-search").val();
-    console.log(citySearch)
+    console.log(citySearch);
+    searchCity(citySearch);
 });
 
 // do this when the search button is clicked
+function searchCity(citySearch){
+    let queryURL = "api.openweathermap.org/data/2.5/weather?q=" + citySearch + "&appid=f261145622245616c003651f582a49a8&units=imperial"
+    
+    // ajax call to openweather api
+    $.ajax({
+        url: queryURL,
+        type: "GET"
+    }).then(function(response){
+        let results = response.data;
+        console.log(results);
 
+    })
+};
 
 
 });
